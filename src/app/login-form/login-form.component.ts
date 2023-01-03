9a8ecb7dccc67f1bd3de8b70756b2525fffeca62
eddy08 <bhardwajharsh08@gmail.com>
2023-01-03 23:22:55 +0530

    Ability to Detect if it is username or email 👁

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -4,17 +4,19 @@ import { FormControl, FormGroup, NgForm } from '@angular/forms';
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+  styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent {
+  form: FormGroup = new FormGroup({
+    user: new FormControl(''),
+    password: new FormControl(''),
+  });
 
-  form:FormGroup=new FormGroup({
-    user:new FormControl(''),
-    password:new FormControl('')
-
-  })
-
-onSubmit(form:NgForm){
-console.log(form)
-}
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
+  isEmail(user: String): Boolean {
+    if (user.includes('@') && user.includes('.com')) return true;
+    else return false;
+  }
 }
